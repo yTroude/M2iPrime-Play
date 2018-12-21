a9e813378c6bf39cf0f56ad3c20d9d281a654546
Yohann Troude <yohann.troude@gmail.com>
2018-12-21 13:35:46 +0100

    Ajout TrackerController

---
+++ app/controllers/TrackerController.java
@@ -0,0 +1,27 @@
+package controllers;
+
+import play.Logger;
+import play.mvc.After;
+import play.mvc.Before;
+import play.mvc.Controller;
+
+import java.util.Map;
+
+public class TrackerController extends Controller {
+
+    @Before
+    static void before() {
+        Logger.info("----------------------");
+        Logger.info("nav - %s", request.url);
+        for (Map.Entry<String, String> entry : request.params.allSimple().entrySet()) {
+            Logger.debug("params : %s = %s", entry.getKey(), entry.getValue());
+        }
+        Logger.info("-----");
+    }
+
+    @After
+    public static void after() {
+        Logger.info("----------------------");
+    }
+
+}
