af404c77346fe64ebee0bc6ab83f6fd1bdcaec95
Matthieu22 <matthieu.martin@viacesi.fr>
2018-12-22 17:59:45 +0100

    Init dev + Niveau de log en debug

--- app/services/UtilisateursService.java
+++ app/services/UtilisateursService.java
@@ -1,9 +1,13 @@
 package services;
 
 import models.UtilisateurDto;
+import play.Logger;
 
 public class UtilisateursService {
-    public static void creerUtilisateur(UtilisateurDto utilisateurDto) {
 
+    public static final String LOG_PREFIX = "UtilisateursService | ";
+
+    public static void creerUtilisateur(UtilisateurDto utilisateurDto) {
+        Logger.debug("%s creerUtilisateur : [%s]", LOG_PREFIX, utilisateurDto.email);
     }
 }
